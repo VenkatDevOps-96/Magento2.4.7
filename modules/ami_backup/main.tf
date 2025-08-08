afe325d93da3f7515eb29fe939e27e9ca27da611
venky <venky@gmail.com>
2025-08-08 20:32:37 +0000

    Fix AMI name formatting to remove invalid characters

--- modules/ami_backup/main.tf
+++ modules/ami_backup/main.tf
@@ -1,8 +1,9 @@
 resource "aws_ami_from_instance" "backup" {
-  name               = "${var.name_prefix}-${timestamp()}"
+  name               = "${var.name_prefix}-${replace(timestamp(), ":", "-")}"
   source_instance_id = var.instance_id
   description        = "Automated backup of instance ${var.instance_id}"
   tags = {
     Name = "AutomatedBackup"
   }
 }
+
